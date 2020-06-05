217f3fd693d983133c1e09f83860dd49a358a5cd
Anik <marufmonem@gmail.com>
2020-06-05 16:31:14 +0600

    Added a bunch of stuff!!
    smooth scrolling, nav bar collapse(small display), nav active on hover.

--- assets/script.js
+++ assets/script.js
@@ -13,8 +13,81 @@ moreLink.addEventListener("click",function(){
     
 })
 
-// $(document).scroll(function () {
-//     var $nav = $("#mainNav");
-//     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-// });
+$(function(){
+    $('#mainNav a').click(function () {
+        $('#mainNav a').removeClass('active');
+        $(this).addClass('active');
+     });
+ });
+
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+
+var navItem = document.getElementsByClassName("nav-item");
+
+// 0
+var glance = document.getElementById("intro");
+glance.addEventListener("mouseover",function(){
+    for(let i=0; i < navItem.length;i++){
+        navItem[i].classList.remove("active");
+    }
+    navItem[0].classList.add("active");  
+
+})
+
+//1
+var education = document.getElementById("education");
+education.addEventListener("mouseover",function(){
+    for(let i=0; i < navItem.length;i++){
+        navItem[i].classList.remove("active");
+    }
+    navItem[1].classList.add("active");  
+
+});
+
+//2
+var job = document.getElementById("job");
+job.addEventListener("mouseover",function(){
+    for(let i=0; i < navItem.length;i++){
+        navItem[i].classList.remove("active");
+    }
+    navItem[2].classList.add("active");  
+
+});
+
+
+//3
+var programming = document.getElementById("programming");
+programming.addEventListener("mouseover",function(){
+    for(let i=0; i < navItem.length;i++){
+        navItem[i].classList.remove("active");
+    }
+    navItem[3].classList.add("active");  
+
+});
+
+var award = document.getElementById("award");
+award.addEventListener("mouseover",function(){
+    for(let i=0; i < navItem.length;i++){
+        navItem[i].classList.remove("active");
+    }
+    navItem[4].classList.add("active");  
+
+})
+
+//5
+var footer = document.getElementById("footer");
+footer.addEventListener("mouseover",function(){
+    for(let i=0; i < navItem.length;i++){
+        navItem[i].classList.remove("active");
+    }
+    navItem[5].classList.add("active");  
+
+})
+
+// $('#education').on('mouseover',function(){
+//     $('nav-item').removeClass("active");
+//     $('nav-item').eq(1).addClass("active");
+// })
 
